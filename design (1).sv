2790e72c73fac1e9f0da22b27a55ba9bc9bfdd2c
harshavardhan249 <harshavardhank.ec23@bmsce.ac.in>
2026-03-02 22:08:18 +0530

    design (1).sv

--- design (1).sv
+++ design (1).sv
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 // File        : digital_clock.sv
-// Author      : Kartik Malipatil / 1BM23EC117
+// Author      : Harshavardhan K / 1BM23EC098
 // Created     : 2026-02-03
 // Module      : digital_clock
 // Project     : SystemVerilog and Verification (23EC6PE2SV)
